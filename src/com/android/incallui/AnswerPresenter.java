f7ae90e8d708bf4e61d267fa4e4ad63781e0d37d
Ravindra <c_rthat@codeaurora.org>
2015-12-16 15:23:20 +0530

    Fix to answer call in DSDS mode.
    Reset the mCall reference to null when a call is disconnected
    only if the disconnected call matches the call present in mcall.
    
    Change-Id: I89dbcfcde1ed313ab7ba859b30bf55aec7555e96
    CRs-Fixed: 945977

--- src/com/android/incallui/AnswerPresenter.java
+++ src/com/android/incallui/AnswerPresenter.java
@@ -253,7 +253,9 @@ public class AnswerPresenter extends Presenter<AnswerPresenter.AnswerUi>
     public void onDisconnect(Call call) {
         int subId = call.getSubId();
         int phoneId = mCalls.getPhoneId(subId);
-        mCall[phoneId] = null;
+        if (call.equals(mCall[phoneId])) {
+            mCall[phoneId] = null;
+        }
     }
 
     public void onSessionModificationStateChange(int sessionModificationState) {
@@ -308,7 +310,7 @@ public class AnswerPresenter extends Presenter<AnswerPresenter.AnswerUi>
         int phoneId = mCalls.getPhoneId(subId);
         mCallId[phoneId] = call.getId();
         mCall[phoneId] = call;
-
+        mCalls.addListener(this);
         // Listen for call updates for the current call.
         mCalls.addCallUpdateListener(mCallId[phoneId], this);
 
